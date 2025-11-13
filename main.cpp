780bb685d97854b8dc67186bf255272c692d60df
Blade-2200 <daniel.lee.sedu@gmail.com>
2025-11-12 17:27:18 -0800

    Finished Problem (Inefficient)
    Used Many for loops to complete problem

--- main.cpp
+++ main.cpp
@@ -8,12 +8,6 @@
 // Return true if the Sudoku board is valid, otherwise return false
 // Note: A board does not need to be full or be solvable to be valid.
 
-Each row must contain the digits 1-9 without duplicates.
-Each column must contain the digits 1-9 without duplicates.
-Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without duplicates.
-Return true if the Sudoku board is valid, otherwise return false
-
-Note: A board does not need to be full or be solvable to be valid.
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
@@ -28,6 +22,8 @@ public:
                     } else { seen.insert(std::string(1, board[i][j])); }
                 }
             }
+        }
+        seen.clear();
         for (int i = 0; i < 3; i++) {
             for (int j = 3; j < 6; j++) {
                 std::cout << std::string(1, board[i][j]);
@@ -39,6 +35,118 @@ public:
                 }
             }   
         }
+        seen.clear();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 6; j < 9; j++) {
+                std::cout << std::string(1, board[i][j]);
+                std::string value(1, board[i][j]);
+                if (board[i][j] != '.') {
+                    if (seen.find(value) != seen.end()) {
+                        return false;
+                    } else { seen.insert(std::string(1, board[i][j])); }
+                }
+            }   
+        }
+        seen.clear();
+        for (int i = 3; i < 6; i++) {
+            for (int j = 0; j < 3; j++) {
+                std::cout << std::string(1, board[i][j]);
+                std::string value(1, board[i][j]);
+                if (board[i][j] != '.') {
+                    if (seen.find(value) != seen.end()) {
+                        return false;
+                    } else { seen.insert(std::string(1, board[i][j])); }
+                }
+            }   
+        }
+        seen.clear();
+        for (int i = 3; i < 6; i++) {
+            for (int j = 3; j < 6; j++) {
+                std::cout << std::string(1, board[i][j]);
+                std::string value(1, board[i][j]);
+                if (board[i][j] != '.') {
+                    if (seen.find(value) != seen.end()) {
+                        return false;
+                    } else { seen.insert(std::string(1, board[i][j])); }
+                }
+            }   
+        }
+        seen.clear();
+        for (int i = 3; i < 6; i++) {
+            for (int j = 6; j < 9; j++) {
+                std::cout << std::string(1, board[i][j]);
+                std::string value(1, board[i][j]);
+                if (board[i][j] != '.') {
+                    if (seen.find(value) != seen.end()) {
+                        return false;
+                    } else { seen.insert(std::string(1, board[i][j])); }
+                }
+            }   
+        }
+        seen.clear();
+        for (int i = 6; i < 9; i++) {
+            for (int j = 0; j < 3; j++) {
+                std::cout << std::string(1, board[i][j]);
+                std::string value(1, board[i][j]);
+                if (board[i][j] != '.') {
+                    if (seen.find(value) != seen.end()) {
+                        return false;
+                    } else { seen.insert(std::string(1, board[i][j])); }
+                }
+            }   
+        }
+        seen.clear();
+        for (int i = 6; i < 9; i++) {
+            for (int j = 3; j < 6; j++) {
+                std::cout << std::string(1, board[i][j]);
+                std::string value(1, board[i][j]);
+                if (board[i][j] != '.') {
+                    if (seen.find(value) != seen.end()) {
+                        return false;
+                    } else { seen.insert(std::string(1, board[i][j])); }
+                }
+            }   
+        }
+        seen.clear();
+        for (int i = 6; i < 9; i++) {
+            for (int j = 6; j < 9; j++) {
+                std::cout << std::string(1, board[i][j]);
+                std::string value(1, board[i][j]);
+                if (board[i][j] != '.') {
+                    if (seen.find(value) != seen.end()) {
+                        return false;
+                    } else { seen.insert(std::string(1, board[i][j])); }
+                }
+            }   
+        }
+
+        for (int i = 0; i < 9; i++) {
+            seen.clear();
+            for (int j = 0; j < 9; j++) {
+                std::string value(1, board[i][j]);
+                if (board[i][j] != '.') {
+                    if (seen.find(value) != seen.end()) {
+                        return false;
+                    } else { seen.insert(std::string(1, board[i][j])); }
+                }
+            }
+        }
+        for (int j = 0; j < 9; j++) {
+            seen.clear();
+            for (int i = 0; i < 9; i++) {
+                if (board[i][j] != '.') {
+                    std::string value(1, board[i][j]);
+                    if (seen.find(value) != seen.end()) {
+                        return false;
+                }
+                seen.insert(value);
+            }
+        }
+    }
+
+        return true;
+    }
+};
         return true;
     }
 };
